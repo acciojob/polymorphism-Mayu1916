1f3de4bbb7c920f39a08a26d5dbe503822a747c9
Mayu1916 <69160936+Mayu1916@users.noreply.github.com>
2023-02-09 13:12:49 +0530

    polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+    class Product {
 
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public double product(double x, double y) {
+            return (double)x*y;
+        }
+
+    }
+    public void main(String []args){
+        Product p = new Product();
+        p.product(2,4);
+        p.product(2.22,3.33);
+    }
 }
